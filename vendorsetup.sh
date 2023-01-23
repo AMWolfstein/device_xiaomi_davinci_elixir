b535eef722e5f74b00657837e0529c9697d71b1d
S ∆ U R ∆ V <dwarmachine24@gmail.com>
2023-01-23 14:06:43 +0530

    davinci: Update build script

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,6 +2,7 @@
 rm -rf vendor/xiaomi
 rm -rf kernel/xiaomi
 rm -rf hardware/xiaomi
+rm -rf hardware/lineage/compat
 
 # clone
 git clone -b 15 --depth=1 https://gitlab.com/PixelOS-Devices/playgroundtc.git prebuilts/clang/host/linux-x86/clang-playground
@@ -9,3 +10,6 @@ git clone -b 13x --depth=1 https://github.com/helliscloser/kernel_xiaomi_sm6150.
 git clone -b 13 https://github.com/ProjectElixir-Devices/vendor_xiaomi_davinci.git vendor/xiaomi
 git clone -b leica https://gitlab.com/ItzDFPlayer/vendor_davinci-miuicamera.git vendor/xiaomi/miuicamera
 git clone -b lineage-20 https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
+git clone --depth=1 https://github.com/LineageOS/android_hardware_lineage_compat.git hardware/lineage/compat
+
+rm -rf hardware/xiaomi/megvii
