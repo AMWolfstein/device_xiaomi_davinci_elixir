278771c74bbe46b4f7c67dc0c2a5b1e19356f83b
AMWolfstein <ahmedhero4040@gmail.com>
2023-02-23 09:58:11 +0200

    Revert "davinci: Let's build with Leica miuicamera"
    This reverts commit f46ef20f65c4d57410bfc780cb398f42afc0a7a2.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,7 +8,6 @@ rm -rf hardware/lineage/compat
 git clone -b 15 --depth=1 https://gitlab.com/PixelOS-Devices/playgroundtc.git prebuilts/clang/host/linux-x86/clang-playground
 git clone -b 13 --depth=1 https://github.com/ProjectElixir-Devices/kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150
 git clone -b 13 https://github.com/ProjectElixir-Devices/vendor_xiaomi_davinci.git vendor/xiaomi
-git clone -b leica https://gitlab.com/ItzDFPlayer/vendor_davinci-miuicamera.git vendor/xiaomi/miuicamera
 git clone -b lineage-20 https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
 git clone --depth=1 https://github.com/LineageOS/android_hardware_lineage_compat.git hardware/lineage/compat
 
